9e733a93743aa2df98d02f53920176040d8e794f
Schizohatter <soanvig@gmail.com>
2017-07-09 22:18:43 +0200

    Default disable grid

--- mm-jsr.js
+++ mm-jsr.js
@@ -30,7 +30,7 @@ class JSRange {
       single:       false,
       prefixes:     {},
       suffixes:     {},
-      grid:         { step: 0.02, bigstepNth: 5, disabled: false, values: true }
+      grid:         { step: 0.02, bigstepNth: 5, disabled: true, values: true }
     }
 
     // Merge default options with supplied options
