9395a226e421f5dd1d6ef5cf60a0491322a34c9c
Schizohatter <soanvig@gmail.com>
2017-10-09 20:29:41 +0200

    Tests: helpers/listeOn

--- tests/helpers.test.js
+++ tests/helpers.test.js
@@ -13,3 +13,43 @@ describe('test calculateDecimals', () => {
     expect(result).toBe(2);
   });
 });
+
+describe('test listenOn', () => {
+  let testElements = [];
+
+  beforeEach(() => {
+    for(let i = 0; i < 2; i += 1) {
+      testElements.push(document.createElement('div'));
+    }
+  });
+  
+  afterEach(() => {
+    testElements = [];
+  });
+
+  test('single element input', () => {
+    let eventLaunched = false;
+    const event = new MouseEvent('click');
+
+    listenOn(testElements[0], 'click', () => {
+      eventLaunched = true;
+    });
+
+    testElements[0].dispatchEvent(event);
+
+    expect(eventLaunched).toBe(true);
+  });
+
+  test('array element input', () => {
+    let eventLaunched = false;
+    const event = new MouseEvent('click');
+
+    listenOn(testElements, 'click', () => {
+      eventLaunched = true;
+    });
+
+    testElements[testElements.length - 1].dispatchEvent(event);
+
+    expect(eventLaunched).toBe(true);
+  });
+});
