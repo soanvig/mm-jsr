137e86ce3f7b36ab805549c9e64afb85a6e4ba1a
Soanvig <soanvig@gmail.com>
2021-03-27 18:21:38 +0100

    [v2] add keyboard support (#59)

--- src/InputHandler.ts
+++ src/InputHandler.ts
@@ -1,19 +1,23 @@
 import { ConfigDto } from '@/models/Config';
+import { StateDto } from '@/models/State';
 import { Value } from '@/models/Value';
 import { StateProcessor } from '@/StateProcessor';
 
 interface Ctor {
   config: ConfigDto;
+  getState: () => StateDto;
   onChange: (index: number, value: Value) => void;
 }
 
 export class InputHandler {
   private config: ConfigDto;
   private onChange: (index: number, value: Value) => void;
+  private getState: () => StateDto;
 
   private constructor (ctor: Ctor) {
     this.config = ctor.config;
     this.onChange = ctor.onChange;
+    this.getState = ctor.getState;
   }
 
   public setRealValue (index: number, value: number) {
@@ -29,6 +33,24 @@ export class InputHandler {
       max: this.config.max,
       min: this.config.min,
       ratio: value,
+    }));
+  }
+
+  public changeRatioBy (index: number, value: number) {
+    const currentValue = this.getState().values[index];
+    this.onChange(index, Value.fromRatio({
+      max: this.config.max,
+      min: this.config.min,
+      ratio: currentValue.asRatio() + value,
+    }));
+  }
+
+  public changeRealBy (index: number, value: number) {
+    const currentValue = this.getState().values[index];
+    this.onChange(index, Value.fromReal({
+      max: this.config.max,
+      min: this.config.min,
+      real: currentValue.asReal() + value,
     }));
   }
 
