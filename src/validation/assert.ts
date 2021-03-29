0c7fb9e5118d9d7b8d22b0ca49ad4797fb8a831d
Mateusz Koteja <soanvig@users.noreply.github.com>
2021-03-29 23:53:58 +0200

    [v2-limit-extension] add limit module and extension (#68)

--- src/validation/assert.ts
+++ src/validation/assert.ts
@@ -21,6 +21,9 @@ export const isInstanceOf = (klass: any): AssertFunction => v =>
 export const isNumber: AssertFunction = v =>
   (Number.isFinite(v) ? false : { error: 'expected number', value: v });
 
+export const isPlainObject: AssertFunction = v =>
+  (isPlainObjectImpl(v) ? false : { error: 'expected literal object', value: v });
+
 export const isArray = (assertFunction: AssertFunction): AssertFunction => v => {
   if (!Array.isArray(v)) {
     return {
@@ -32,4 +35,12 @@ export const isArray = (assertFunction: AssertFunction): AssertFunction => v => 
   const assertions = v.map(assertFunction);
 
   return assertions.find(Boolean) ?? false;
+};
+
+// source: https://stackoverflow.com/questions/5876332/how-can-i-differentiate-between-an-object-literal-other-javascript-objects
+const isPlainObjectImpl = (obj: any) => {
+  return typeof obj === 'object' // separate from primitives
+      && obj !== null // is obvious
+      && obj.constructor === Object // separate instances (Array, DOM, ...)
+      && Object.prototype.toString.call(obj) === '[object Object]'; // separate build-in like Math
 };
