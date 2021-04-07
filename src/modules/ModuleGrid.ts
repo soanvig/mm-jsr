fdb066fcd79b9f60ba07aa21aa79425c34a4f299
Soanvig <soanvig@gmail.com>
2021-04-07 21:18:46 +0200

    [v2-api] allow to configure grid settings

--- src/modules/ModuleGrid.ts
+++ src/modules/ModuleGrid.ts
@@ -3,9 +3,30 @@ import { StateDto } from '@/models/State';
 import { Value } from '@/models/Value';
 import { Module } from '@/modules/Module';
 
+interface Settings {
+  color: string;
+  height: number;
+  fontSize: number;
+  fontFamily: string;
+  textPadding: number;
+}
+
 export class ModuleGrid extends Module {
   private grid!: HTMLCanvasElement;
   private context!: CanvasRenderingContext2D;
+  private settings: Settings;
+
+  constructor (settings: Partial<Settings> = {}) {
+    super();
+
+    this.settings = Object.assign({
+      color: 'rgba(0, 0, 0, 0.3)',
+      height: 10,
+      fontSize: 10,
+      fontFamily: 'sans-serif',
+      textPadding: 5,
+    }, settings);
+  }
 
   public destroy () {
     this.grid.remove();
@@ -34,32 +55,24 @@ export class ModuleGrid extends Module {
   }
 
   private drawGrid () {
-    const defaults = {
-      color: 'rgba(0, 0, 0, 0.3)',
-      height: 10,
-      fontSize: 10,
-      fontFamily: 'sans-serif',
-      textPadding: 5,
-    };
-
     const context = this.context;
     const numberOfLines = 100;
     const ratio = 1 / numberOfLines;
 
     const pixelRatio = window.devicePixelRatio || 1;
     const canvasWidth = this.grid.offsetWidth * pixelRatio;
-    const canvasHeight = (defaults.height + defaults.fontSize + defaults.textPadding) * pixelRatio;
+    const canvasHeight = (this.settings.height + this.settings.fontSize + this.settings.textPadding) * pixelRatio;
 
     this.grid.width = canvasWidth;
     this.grid.height = canvasHeight;
 
     context.scale(pixelRatio, pixelRatio);
 
-    context.clearRect(0, 0, canvasWidth, defaults.height);
+    context.clearRect(0, 0, canvasWidth, this.settings.height);
     context.beginPath();
     context.lineWidth = 1;
-    context.fillStyle = context.strokeStyle = defaults.color;
-    context.font = `${defaults.fontSize}px ${defaults.fontFamily}`;
+    context.fillStyle = context.strokeStyle = this.settings.color;
+    context.font = `${this.settings.fontSize}px ${this.settings.fontFamily}`;
     context.textBaseline = 'top';
 
     for (let i = 0; i <= numberOfLines; i += 1) {
@@ -67,7 +80,7 @@ export class ModuleGrid extends Module {
       let left = i * ratio * canvasWidth;
       left = Math.round(left * 100.0) / 100.0;
       context.moveTo(left, 0);
-      context.lineTo(left, defaults.height);
+      context.lineTo(left, this.settings.height);
 
       // Draw text
       if (i % 10 === 0) {
@@ -89,7 +102,7 @@ export class ModuleGrid extends Module {
         const roundedValue = value.changeReal(Math.round(value.asReal() / this.config.step) * this.config.step);
 
         const text = this.config.formatter(roundedValue.asReal());
-        context.fillText(text.toString(), i * ratio * canvasWidth, defaults.height + defaults.textPadding);
+        context.fillText(text.toString(), i * ratio * canvasWidth, this.settings.height + this.settings.textPadding);
       }
     }
 
