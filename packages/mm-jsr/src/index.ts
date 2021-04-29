4acfa5d68f908a05f6643e32724e0bcf65d6c33b
Soanvig <soanvig@gmail.com>
2021-04-30 00:20:25 +0200

    [react-adapter] wip

--- packages/mm-jsr/src/index.ts
+++ packages/mm-jsr/src/index.ts
@@ -1,3 +1,4 @@
 import { JSR } from './JSR';
 
+export { JsrConstructor } from './JSR';
 export default JSR;
---
+++ packages/react-mm-jsr/src/index.ts
@@ -0,0 +1,66 @@
+import JSR from 'mm-jsr';
+import React, { useEffect, useRef } from 'react';
+
+type Ctor = ConstructorParameters<typeof JSR>[0];
+type Config = Omit<Ctor['config'], 'container'>;
+type Modules = Ctor['modules'];
+
+export interface Props {
+  className?: string;
+  config: Config;
+  modules: Modules;
+  disabled?: boolean;
+  onChange?: ((v: { index: number, real: number, ratio: number }) => void);
+}
+
+export const ReactJSR = ({
+  className,
+  config,
+  modules,
+  disabled,
+  onChange,
+}: Props) => {
+  const containerRef = useRef<HTMLElement>();
+  const jsrRef = useRef<JSR>();
+
+  useEffect(() => {
+    if (jsrRef.current) {
+      jsrRef.current.destroy();
+    }
+  }, []);
+
+  useEffect(() => {
+    if (containerRef.current) {
+      jsrRef.current = new JSR({
+        config: {
+          container: containerRef.current,
+          ...config,
+        },
+        modules,
+      });
+
+      jsrRef.current.onValueChange(v => {
+        if (onChange) {
+          onChange(v);
+        }
+      });
+    }
+  }, [containerRef.current]);
+
+  useEffect(() => {
+    if (!jsrRef.current) {
+      return;
+    }
+
+    if (disabled) {
+      jsrRef.current.disable();
+    } else {
+      jsrRef.current.enable();
+    }
+  }, [disabled, jsrRef.current]);
+
+  return React.createElement('div', {
+    className,
+    ref: containerRef,
+  });
+};
