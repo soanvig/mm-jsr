653ad71f4b5ce718b89194be18622761b91676b7
Soanvig <soanvig@gmail.com>
2026-06-27 15:30:14 +0200

    refactor: migrate webpage to html

--- packages/webpage/rollup.config.js
+++ packages/webpage/rollup.config.js
@@ -1,4 +1,3 @@
-import svelte from 'rollup-plugin-svelte';
 import resolve from '@rollup/plugin-node-resolve';
 import terser from '@rollup/plugin-terser';
 
@@ -6,20 +5,12 @@ export default {
   input: 'src/main.js',
   output: {
     file: 'public/build/bundle.js',
-    format: 'iife'
+    format: 'iife',
   },
   plugins: [
-    svelte({
-      emitCss: false,
-      compilerOptions: {
-      }
-    }),
     resolve({
       browser: true,
-      exportConditions: ['svelte'],
-      extensions: ['.svelte']
     }),
-    terser({
-    }),
-  ]
-}
+    terser({}),
+  ],
+};
